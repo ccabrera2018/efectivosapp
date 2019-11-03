1c46ccf39f2241e9d848bf7062e730e11f0284a0
unknown <cesar cabrera>
2019-11-03 00:19:22 -0600

    merketingSeo

---
+++ src/main/java/com/marketingseo/app/MarketingSeoApplication.java
@@ -0,0 +1,13 @@
+package com.marketingseo.app;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MarketingSeoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MarketingSeoApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/marketingseo/app/MarketingSeoApplicationTests.java
@@ -0,0 +1,16 @@
+package com.marketingseo.app;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class MarketingSeoApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
